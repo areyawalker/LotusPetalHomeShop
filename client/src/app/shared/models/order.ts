46c991224c144150d8045145cf854682625d6d2c
Lynn Walker <lynn@winmedia.com>
2022-04-10 12:40:27 -0500

    Initial Commit

---
+++ client/src/app/shared/models/order.ts
@@ -0,0 +1,30 @@
+import { IAddress } from "./address";
+
+export interface IOrderToCreate {
+    basketId: string;
+    deliveryMethodId: number;
+    shipToAddress: IAddress;
+}
+
+export interface IOrder {
+    id: number;
+    buyerEmail: string;
+    orderDate: string;
+    shipToAddress: IAddress;
+    deliveryMethod: string;
+    shippingPrice: number;
+    orderItems: IOrderItem[];
+    subtotal: number;
+    status: string;
+    total: number;
+  }
+  
+  export interface IOrderItem {
+    id: number;
+    productName: string;
+    price: number;
+    quantity: number;
+    pictureUrl: string;
+    brand: string;
+    type: string;
+  }
